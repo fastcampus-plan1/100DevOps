58f76e7d13d5e2bebea0f1612c8d2b35a054a5ab
오성근 <osk@mensakorea.org>
2024-01-14 16:01:02 +0900

    Create 26.Network_monitoring.sh

---
+++ Part1/26.Network_monitoring.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOG=ping.log
+PREFIX=192.168.1
+for i in $(seq 254)
+do
+  ping -c1 -w1 ${PREFIX}.$i && \
+    echo "${PREFIX}.$i is alive" | tee -a $LOG || \
+    echo "${PREFIX}.$i is dead" | tee -a $LOG &
+done
